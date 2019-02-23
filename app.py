22b68a91441f714c6b9e6d2f9ae82b6eeb333310
chiehhsi <j4500123@gmail.com>
2019-02-23 12:32:00 +0800

    ,

--- app.py
+++ app.py
@@ -41,10 +41,6 @@ def callback():
     return 'OK'
 
 # 處理訊息
-@handler.default()
-def default(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text= "Say Hi to Me :)")
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("event.reply_token:", event.reply_token)
